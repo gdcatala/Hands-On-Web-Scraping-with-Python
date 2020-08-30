c9d31607302f56b68ebff3c9cebd1470dfde7d80
gdcatala <gdcatala@gmail.com>
2020-08-30 18:25:11 -0400

    Changes on the examples because we are running Ubuntu 18.04 LTS

--- Chapter02/wikipedia_content.py
+++ Chapter02/wikipedia_content.py
@@ -1,12 +1,13 @@
 import requests
-import os
+
 link = "https://en.wikipedia.org/wiki/List_of_most_popular_websites"
 response = requests.get(link)
-print(type(response))
-content = response.content
-#print(content)
-#Create a html file with the content received as 'content'
-file = open(os.getcwd()+os.sep+"tests"+os.sep+"wikicontent.html","wb")
-file.write(content)
-file.close()
-#print(content)  
+#*print(type(response))
+# the response.content method retreives a byte object
+#*content = response.content
+# the response.txt method retreives  a string object
+content_string = response.text
+#*print(content[0:150])
+
+with open("wikipedia_content", "w") as file:
+    file.write(content_string)
