318941a5e746c6cbdbac2e20aa96241a240f43c9
gdcatala <gdcatala@gmail.com>
2020-09-09 21:37:02 -0300

    Update of the files

--- Chapter02/request_examples.py
+++ Chapter02/request_examples.py
@@ -28,4 +28,23 @@ addedheaders = {"user-agent":""}
 #r1 = requests.get(link2, params=queries, headers=addedheaders)
 #print(r.url)
 
-#Through the POST method the resource state can be altered.
+#Through the POST method the resource state can be altered. Data that's
+#posted or sent to the requested URL is not visible in the URL, instead
+#it's transfered to the request body. A request that's made using POST
+#isn't cached or bookmarked and has no restrictions in terms of length
+
+
+params = {'custname':'Mr. ABC','custtel':'','custemail':'abc@somedomain.com',
+'size':'small','topping':['cheese','mushroom'],'delivery':'13:00','comments':'None'}
+
+headers={'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image'
+         '/webp,image/apng,*/*;q=0.8', 'Content-Type':'application/x-www-form-urlencoded',
+         'Referer':'http://httpbin.org/forms/post'}
+
+#Making POST request to postURL with params and request headers,
+#reponse will be read as JSON
+
+post_response = requests.post('http://httpbin.org/post',data=params,headers=headers).json()
+print(post_response)
+
+
--- Chapter02/urllib_http_headers.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-
-url='https://www.samsclub.com/sitemap.xml'
-someRequest = urllib.request.urlopen(url)#loads provided URL
-someRequest.getheaders() #Lists all HTTP headers.
-someRequest.getheader("Content-Type") #return value of header 'Content-Type'
